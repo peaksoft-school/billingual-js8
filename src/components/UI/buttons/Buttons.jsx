7b1678fca09e04f630d22a0960ba84bbb5282f92
Mairamkul <mairamkulormonova@gmail.com>
2023-04-07 18:38:58 +0600

    create reusable buttons

---
+++ src/components/UI/buttons/Buttons.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { styled } from '@mui/material/styles'
+import MuiButton from '@mui/material/Button'
+
+const BaseButton = styled(MuiButton)(({}) => ({
+   display: 'flex',
+   alignItems: 'center',
+   borderRadius: '8px',
+}))
+
+const Button = ({ children, variant, onClick, disabled, ...rest }) => {
+   return (
+      <BaseButton
+         onClick={onClick}
+         variant={variant}
+         disabled={disabled}
+         {...rest}
+      >
+         {children}
+      </BaseButton>
+   )
+}
+
+export default Button
