7d3443804478319d5bfc68488161fea6388a6433
Anarbek <AnarbekIshembekUulu>
2023-04-09 08:29:25 +0600

    landing-page-part-1

---
+++ src/components/landing-page/InfoSection.jsx
@@ -0,0 +1,545 @@
+import { styled } from '@mui/material'
+import img1 from '../../assets/images/c1f1f2ed1497825e71058d467b8ec831 1.png'
+import img2 from '../../assets/images/4be617c3542970e193ff464c3292e0a5 1.png'
+import img3 from '../../assets/images/3f5986b9c2254548566619450f4f6064 1.png'
+import accessible from '../../assets/images/accessible.png'
+import speech from '../../assets/images/speech.png'
+import extensive from '../../assets/images/extensive.png'
+import tutoring from '../../assets/images/tutoring.png'
+import booksImage from '../../assets/images/image.png'
+import OurTeamImg1 from '../../assets/images/1.jpg'
+import OurTeamImg2 from '../../assets/images/2.jpg'
+import OurTeamImg3 from '../../assets/images/3.jpg'
+import OurTeamImg4 from '../../assets/images/4.jpg'
+import OurTeamImg5 from '../../assets/images/5.jpg'
+import OurTeamImg6 from '../../assets/images/6.jpg'
+
+const Card = styled('div')(() => ({
+   position: 'absolute',
+   width: '100%',
+   height: '200%',
+   paddingTop: '120px',
+   marginTop: '778px',
+   background: '#f0f0dc',
+}))
+
+const InfoCard = styled('div')(() => ({
+   height: '248px',
+   width: '1203px',
+   margin: '0 auto',
+   borderRadius: '0px',
+   display: 'flex',
+   flexdirection: '',
+   padding: '0px',
+}))
+
+const Img1 = styled('img')(() => ({
+   width: '300.96px',
+   height: '176px',
+   left: '135px',
+   top: '898px',
+}))
+
+const Img2 = styled('img')(() => ({
+   width: ' 300.96px',
+   height: '176px',
+   left: '586px',
+   top: '898px',
+}))
+
+const Img3 = styled('img')(() => ({
+   width: ' 300.96px',
+   height: '176px',
+   left: '1020px',
+   top: '898px',
+}))
+
+const Img1DivOne = styled('div')(() => ({
+   width: '335px',
+   height: '248px',
+   left: '118px',
+   top: '898px',
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+}))
+const Img1DivSecond = styled('div')(() => ({
+   width: '326px',
+   height: '248px',
+   marginLeft: '120px',
+   top: '898px',
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+}))
+const Img1DivThird = styled('div')(() => ({
+   width: '302px',
+   height: '248px',
+   marginLeft: '120px',
+   top: '898px',
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+}))
+
+const Over = styled('p')(() => ({
+   width: '335px',
+   height: '48px',
+   left: '118px',
+   marginTop: '23px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '400',
+   fontSize: '16px',
+   lineHeight: '24px',
+   textAlign: 'center',
+   color: '#23212A',
+}))
+
+const StudentsOver = styled('div')(() => ({
+   width: '326px',
+   height: '48px',
+   left: '573px',
+   marginTop: '23px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '400',
+   fontSize: '16px',
+   lineHeight: '24px',
+   textAlign: 'center',
+   color: '#23212A',
+}))
+
+const Eligble = styled('div')(() => ({
+   width: '302px',
+   height: '48px',
+   left: '1019px',
+   marginTop: '23px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '400',
+   fontSize: '16px',
+   lineHeight: '24px',
+   textAlign: 'center',
+   color: '#23212A',
+}))
+
+const Description = styled('div')(() => ({
+   width: '1205.81px',
+   height: ' 437px',
+   margin: '0 auto',
+   marginLeft: '110px',
+   marginTop: '120px',
+   display: 'flex',
+   marginBottom: '121.39px',
+}))
+
+const UserExpiriance = styled('div')(() => ({
+   fontFamily: 'Gilroy',
+   fontSize: '40px',
+   fontWeight: '700',
+   lineHeight: '48px',
+   letterSpacing: '0em',
+   textAlign: 'left',
+   left: '5.62%',
+   right: '71.6%',
+   top: '15.78%',
+   bottom: '83.02%',
+   height: '96px',
+   width: '328px',
+   color: '#3752B4',
+}))
+
+const DescriptionText = styled('div')(() => ({
+   width: '640px',
+   height: '120px',
+   left: '81px',
+   top: '1396px',
+   marginTop: '34px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: 400,
+   fontSize: '1rem',
+   lineHeight: '24px',
+   color: '#23212A',
+}))
+
+const ImgAccessible = styled('img')(() => ({
+   width: ' 44.12px',
+   height: '50px',
+}))
+
+const ImgAccessibleText = styled('div')(() => ({
+   width: '160px',
+   height: '44px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: 400,
+   fontSize: '16px',
+   lineHeight: '22px',
+   color: '#23212A',
+   top: '43px',
+}))
+
+const DivAccessible = styled('div')(() => ({
+   display: 'flex',
+   gap: '28.58px',
+   alignItems: 'center',
+   marginTop: '40px',
+}))
+
+const ImgSpeech = styled('img')(() => ({
+   width: '39px',
+   height: '50px',
+}))
+
+const ImgSpeechText = styled('div')(() => ({
+   width: '160px',
+   height: '44px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: 400,
+   fontSize: '16px',
+   lineHeight: ' 140%',
+   color: '#23212A',
+}))
+
+const DivSpeech = styled('div')(() => ({
+   display: 'flex',
+   gap: '28.58px',
+   alignItems: 'center',
+   marginTop: '48.98px',
+}))
+
+const ImgExtensive = styled('img')(() => ({
+   width: ' 54.44px',
+   height: ' 50px',
+}))
+
+const ImgExtensiveText = styled('div')(() => ({
+   width: '160px',
+   height: '44px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: 400,
+   fontSize: '16px',
+   lineHeight: '140%',
+   color: '#23212A',
+   top: '43px',
+}))
+
+const DivExtensive = styled('div')(() => ({
+   display: 'flex',
+   gap: '28.58px',
+   alignItems: 'center',
+   marginTop: '40px',
+}))
+
+const ImgTutoring = styled('img')(() => ({
+   width: ' 55.56px',
+   height: '50px',
+}))
+
+const TutoringText = styled('div')(() => ({
+   width: '160px',
+   height: '44px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: 400,
+   fontSize: '16px',
+   lineHeight: '140%',
+   color: '#23212A',
+}))
+
+const DivTutoring = styled('div')(() => ({
+   display: 'flex',
+   gap: '28.58px',
+   alignItems: 'center',
+   marginTop: '48.98px',
+}))
+
+const ImgBook = styled('img')(() => ({
+   width: '460.81px',
+   height: '420px',
+   marginLeft: '104px',
+   marginTop: '17px',
+   marginRight: '154.19px',
+}))
+const DivSeparation = styled('div')(() => ({
+   display: 'flex',
+   gap: '67.88px',
+}))
+const OurTeam = styled('div')(() => ({
+   width: '1232px',
+   height: '335px',
+   marginTop: '1045px',
+   margin: '0 auto',
+}))
+
+const TextOurTeam = styled('div')(() => ({
+   width: '180px',
+   height: '52px',
+   margin: '0 auto',
+   top: '1823px',
+   fontFamily: 'Gilroy',
+   fontStyle: 'normal',
+   fontWeight: 700,
+   fontSize: '40px',
+   lineHeight: ' 130%',
+   textTransform: 'capitalize',
+   color: '#3752B4',
+}))
+const OurTeamImage1 = styled('img')(() => ({
+   width: '180px',
+   height: '180px',
+   left: '104px',
+   top: '1922px',
+   borderRadius: '0px 0px 0px 40px',
+}))
+const OurTeamImage2 = styled('img')(() => ({
+   width: '180px',
+   height: '180px',
+   left: '104px',
+   borderRadius: '40px 0px',
+   marginRight: '30px',
+}))
+const OurTeamImage3 = styled('img')(() => ({
+   width: '180px',
+   height: '180px',
+   left: '104px',
+   borderRadius: ' 0px 0px 40px 0px',
+   marginRight: '32px',
+}))
+const OurTeamImage4 = styled('img')(() => ({
+   width: '180px',
+   height: '180px',
+   left: '104px',
+   borderRadius: '40px 0px 0px 0px',
+   marginRight: '30px',
+}))
+const OurTeamImage5 = styled('img')(() => ({
+   width: '180px',
+   height: '180px',
+   left: '104px',
+   borderRadius: '40px 0px',
+   marginRight: '30px',
+}))
+const OurTeamImage6 = styled('img')(() => ({
+   width: '180px',
+   height: '180px',
+   left: '104px',
+   borderRadius: '0px 40px 0px 0px',
+   marginRight: '30px',
+}))
+const DivImage = styled('div')(() => ({
+   display: 'flex',
+   marginTop: '48px',
+}))
+const DivImageEmployeeName = styled('div')(() => ({
+   display: 'flex',
+   flexDirection: 'column',
+   width: '180px',
+   height: '235px',
+   marginRight: '30px',
+}))
+
+const DivEmployeeNamePerson = styled('div')(() => ({
+   height: '41px',
+   width: '124px',
+   margin: '0 auto',
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+   textAlign: 'center',
+   marginTop: '14px',
+}))
+const NamePersonEmployee = styled('div')(() => ({
+   width: '97px',
+   height: '21px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '600',
+   fontSize: '16px',
+   lineHeight: '130%',
+   color: '#3A10E5',
+}))
+const NamePersonEmployee2 = styled('div')(() => ({
+   width: '94px',
+   height: '21px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '600',
+   fontSize: '16px',
+   lineHeight: '130%',
+   color: '#3A10E5',
+}))
+const NamePersonEmployee3 = styled('div')(() => ({
+   width: '98px',
+   height: '21px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '600',
+   fontSize: '16px',
+   lineHeight: '130%',
+   color: '#3A10E5',
+}))
+const NamePersonEmployee4 = styled('div')(() => ({
+   width: '105px',
+   height: '21px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '600',
+   fontSize: '16px',
+   lineHeight: '130%',
+   color: '#3A10E5',
+}))
+const NamePersonEmployee5 = styled('div')(() => ({
+   width: '110px',
+   height: '21px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '600',
+   fontSize: '16px',
+   lineHeight: '130%',
+   color: '#3A10E5',
+}))
+
+const NamePersonEmployee6 = styled('div')(() => ({
+   width: '115px',
+   height: '21px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '600',
+   fontSize: '16px',
+   lineHeight: '130%',
+   color: '#3A10E5',
+}))
+const Employee = styled('div')(() => ({
+   width: '124px',
+   height: '18px',
+   marginTop: '2px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: 400,
+   fontSize: '14px',
+   lineHeight: '130%',
+   textAlign: 'center',
+   color: '#020202',
+}))
+
+const InfoSection = () => {
+   return (
+      <Card>
+         <InfoCard>
+            <Img1DivOne>
+               <Img1 src={img1} />
+               <Over>
+                  Over 10,000 fee waivers for the Bilingual English Test are
+                  offered annually.
+               </Over>
+            </Img1DivOne>
+            <Img1DivSecond>
+               <Img2 src={img2} />
+               <StudentsOver>
+                  Students from over 200 countries and territories have
+                  benefitted.
+               </StudentsOver>
+            </Img1DivSecond>
+            <Img1DivThird>
+               <Img3 src={img3} />
+               <Eligble>
+                  Eligible students can take the test with absolutely zero cost
+                  to them.
+               </Eligble>
+            </Img1DivThird>
+         </InfoCard>
+         <Description>
+            <div>
+               <UserExpiriance>Unparalleled user experience</UserExpiriance>
+               <DescriptionText>
+                  The most effective way to perfect a language is by immersing
+                  yourself in it. Rosetta Stone for Enterprise delivers an
+                  effective end-to-end experience, founded on a wealth of
+                  carefully structured content. Each learner has the opportunity
+                  to balance independent study with optional online tutoring in
+                  a way that fits their schedule and language learning goals.
+               </DescriptionText>
+               <DivSeparation>
+                  <DivAccessible>
+                     <ImgAccessible src={accessible} />
+                     <ImgAccessibleText>
+                        Accessible anytime, anywhere
+                     </ImgAccessibleText>
+                  </DivAccessible>
+                  <DivExtensive>
+                     <ImgExtensive src={extensive} />
+                     <ImgExtensiveText>
+                        Extensive business content
+                     </ImgExtensiveText>
+                  </DivExtensive>
+               </DivSeparation>
+               <DivSeparation>
+                  <DivSpeech>
+                     <ImgSpeech src={speech} />
+                     <ImgSpeechText>Leading speech recognition</ImgSpeechText>
+                  </DivSpeech>
+                  <DivTutoring>
+                     <ImgTutoring src={tutoring} />
+                     <TutoringText>Extensive business content</TutoringText>
+                  </DivTutoring>
+               </DivSeparation>
+            </div>
+            <ImgBook src={booksImage} />
+         </Description>
+         <OurTeam>
+            <TextOurTeam>Our Team</TextOurTeam>
+            <DivImage>
+               <DivImageEmployeeName>
+                  <OurTeamImage1 src={OurTeamImg1} />
+                  <DivEmployeeNamePerson>
+                     <NamePersonEmployee>Alice Archie</NamePersonEmployee>
+                     <Employee>Founder Bilingual</Employee>
+                  </DivEmployeeNamePerson>
+               </DivImageEmployeeName>
+               <DivImageEmployeeName>
+                  <OurTeamImage2 src={OurTeamImg2} />
+                  <DivEmployeeNamePerson>
+                     <NamePersonEmployee2>Mia George</NamePersonEmployee2>
+                     <Employee>Marketer</Employee>
+                  </DivEmployeeNamePerson>
+               </DivImageEmployeeName>
+               <DivImageEmployeeName>
+                  <OurTeamImage3 src={OurTeamImg3} />
+                  <DivEmployeeNamePerson>
+                     <NamePersonEmployee3> Oscar Ryan</NamePersonEmployee3>
+                     <Employee>Developer</Employee>
+                  </DivEmployeeNamePerson>
+               </DivImageEmployeeName>
+               <DivImageEmployeeName>
+                  <OurTeamImage4 src={OurTeamImg4} />
+                  <DivEmployeeNamePerson>
+                     <NamePersonEmployee4>Jack William</NamePersonEmployee4>
+                     <Employee>UX/UI Designer</Employee>
+                  </DivEmployeeNamePerson>
+               </DivImageEmployeeName>
+               <DivImageEmployeeName>
+                  <OurTeamImage5 src={OurTeamImg5} />
+                  <DivEmployeeNamePerson>
+                     <NamePersonEmployee5>Rose Thomas</NamePersonEmployee5>
+                     <Employee>Chief Editor</Employee>
+                  </DivEmployeeNamePerson>
+               </DivImageEmployeeName>
+               <DivImageEmployeeName>
+                  <OurTeamImage6 src={OurTeamImg6} />
+                  <DivEmployeeNamePerson>
+                     <NamePersonEmployee6>Albert Stanley</NamePersonEmployee6>
+                     <Employee>Chief Editor</Employee>
+                  </DivEmployeeNamePerson>
+               </DivImageEmployeeName>
+            </DivImage>
+         </OurTeam>
+      </Card>
+   )
+}
+
+export default InfoSection
---
+++ src/components/landing-page/LandingPageSectionOne.jsx
@@ -0,0 +1,159 @@
+import { Button, styled } from '@mui/material'
+import logo from '../../assets/logo/Layer 1.png'
+import imageBubble from '../../assets/images/Group.png'
+import cap from '../../assets/images/Group 4264.png'
+import imageBook from '../../assets/images/Group 4265.png'
+import InfoSection from './InfoSection'
+
+const LandingPage = styled('div')(() => ({
+   height: '778px',
+   width: '100%',
+   bottom: ' 90.3%',
+   background: ' #FCD200',
+   zIndex: '-44',
+}))
+
+const Logo = styled('img')(() => ({
+   position: 'absolute',
+   height: '48px',
+   left: '90px',
+   right: '78.13%',
+   top: '24px',
+   bottom: '99.1%',
+}))
+
+const LogoAndButtonDiv = styled('div')(() => ({
+   display: 'flex',
+}))
+
+const ButtonDiv = styled('div')(() => ({
+   position: 'absolute',
+   width: '16.1875rem',
+   height: '42px',
+   right: '80px',
+   top: '27px',
+}))
+const ButtonToComeIn = {
+   marginRight: '24px',
+}
+const ImgBubble = styled('img')(() => ({
+   height: '659.9998779296875px',
+   width: '100%',
+   position: 'absolute',
+   top: ' 1.47%',
+   bottom: ' 90.3%',
+   mixBlendMode: 'overlay',
+   marginTop: '107px',
+}))
+const EnglishProficiency = styled('h3')(() => ({
+   position: 'absolute',
+   width: '635px',
+   height: '219px',
+   top: '120px',
+   fontFamily: 'Gilroy',
+   fontStyle: 'normal',
+   fontWeight: '700',
+   fontSize: '60px',
+   lineHeight: '73px',
+   color: ' #43404E',
+}))
+
+const DivInfo = styled('div')(() => ({
+   marginLeft: '90px',
+   lineHeight: '73px',
+}))
+const Bilingual = styled('div')(() => ({
+   position: 'absolute',
+   width: '295px',
+   height: '75px',
+   marginleft: '80px',
+   top: '339px',
+   fontFamily: 'Gilroy',
+   fontStyle: 'normal',
+   fontWeight: ' 900',
+   fontSize: '60px',
+   lineHeight: '75px',
+   color: '#C93D7D',
+}))
+
+const TextDivInfo = styled('div')(() => ({
+   position: 'absolute',
+   width: '772px',
+   height: '60px',
+   marginleft: '80px',
+   top: '440px',
+   fontFamily: 'Poppins',
+   fontStyle: 'normal',
+   fontWeight: '400',
+   fontSize: '20px',
+   lineHeight: '30px',
+   color: '#23212A',
+}))
+
+const AcademicCap = styled('img')(() => ({
+   position: 'absolute',
+   width: ' 236.09px',
+   height: '243px',
+   left: '869px',
+   top: '189px',
+}))
+
+const ImageBooks = styled('img')(() => ({
+   position: 'absolute',
+   width: '594.78px',
+   height: '499px',
+   marginLeft: '820px',
+   marginTop: '279px',
+   zIndex: 10,
+}))
+
+const ButtonToBegin = styled('button')(() => ({
+   height: '59.99999237060547px',
+   width: ' 200px',
+   marginleft: '80px',
+   top: '530px',
+   borderRadius: '0px',
+   position: 'absolute',
+   bottom: ' 92.65%',
+   background:
+      'linear-gradient(186.36deg, #CB4080 5.01%, #C93D7D 29.76%, #BD3172 55.4%, #AD1E60 94.99%)',
+   boxShadow: 'inset 4px -5px 0px rgba(255, 204, 191, 0.68)',
+}))
+
+const LandingPageSectionOne = () => {
+   return (
+      <LandingPage>
+         <LogoAndButtonDiv>
+            <Logo src={logo} />
+
+            <ButtonDiv>
+               <Button sx={ButtonToComeIn} variant="contained">
+                  To come in
+               </Button>
+               <Button variant="contained">Register</Button>
+            </ButtonDiv>
+         </LogoAndButtonDiv>
+         <ImgBubble src={imageBubble} />
+         <DivInfo>
+            <div>
+               <EnglishProficiency>
+                  Prove your English proficiency today with
+               </EnglishProficiency>
+               <Bilingual>BILINGUAL</Bilingual>
+            </div>
+
+            <TextDivInfo>
+               For nearly 30 years, learners have turned to Rosetta Stone to
+               build the fluency and confidence they need to speak new
+               languages.
+            </TextDivInfo>
+            <ButtonToBegin>Register</ButtonToBegin>
+         </DivInfo>
+         <AcademicCap src={cap} />
+         <ImageBooks src={imageBook} />
+         <InfoSection />
+      </LandingPage>
+   )
+}
+
+export default LandingPageSectionOne
