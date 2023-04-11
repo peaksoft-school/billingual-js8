48d4cd1970047d3d7118392d1f917eb45607b82b
Kaukhar Tilekovna <kaukhartilekovna@Kaukhars-MacBook-Pro.local>
2023-04-11 17:03:58 +0600

    added props (...rest)

--- src/components/UI/checkbox/RadioButton.jsx
+++ src/components/UI/checkbox/RadioButton.jsx
@@ -1,7 +1,9 @@
 import { Radio } from '@mui/material'
 
-const RadioButtons = ({ checked, onChange, value }) => {
-   return <Radio checked={checked} onChange={onChange} value={value} />
+const RadioButtons = ({ checked, onChange, value, ...rest }) => {
+   return (
+      <Radio checked={checked} onChange={onChange} value={value} {...rest} />
+   )
 }
 
 export default RadioButtons
