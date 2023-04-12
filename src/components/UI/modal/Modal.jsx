b356e45f86a722d069b9ac956742fbddabc2b20e
Mairamkul <mairamkulormonova@gmail.com>
2023-04-12 16:55:07 +0600

    create reusable  modal

---
+++ src/components/UI/modal/Modal.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
+import { styled } from '@mui/material'
+
+const MyModal = ({ isClose, isOpen, children }) => {
+   return (
+      <Modal open={isOpen} onClose={isClose}>
+         <BoxStyle>{children}</BoxStyle>
+      </Modal>
+   )
+}
+
+export default MyModal
+const BoxStyle = styled(Box)(({ theme }) => ({
+   padding: '36px 89px 36px 89px',
+   position: 'absolute',
+   top: '45%',
+   left: '50%',
+   transform: 'translate(-50%, -50%)',
+   backgroundColor: theme.palette.primary.white,
+   borderRadius: '10px',
+   boxShadow: '0px 4px 39px -5px rgba(196, 196, 196, 0.6)',
+}))
