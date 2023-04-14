3a5ff60527ac67ff10bf7e60b360aa25aa3656cd
Kaukhar Tilekovna <kaukhartilekovna@Kaukhars-MacBook-Pro.local>
2023-04-14 11:30:09 +0600

    Resolved conflict

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import LearnMore from './components/landing-page/LearnMore'
-import UsefulVideos from './components/landing-page/UsefulVideos'
-
 function App() {
-   return (
-      <div>
-         Bilingual
-         <UsefulVideos />
-         <LearnMore />
-      </div>
-   )
+   return <div>Bilingual</div>
 }
 
 export default App
