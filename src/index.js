8e7464f8e779f1f9f9e9eb03d101d3e081e48b6a
Aziat <abdimalikovaziat@gmail.com>
2023-04-15 15:57:31 +0600

    create drag and drop

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './font.css'
 import { ThemeProvider } from '@mui/material'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { theme } from './utils/constants/theme'
@@ -10,9 +12,11 @@ import { theme } from './utils/constants/theme'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-         <App />
-      </ThemeProvider>
+      <DndProvider backend={HTML5Backend}>
+         <ThemeProvider theme={theme}>
+            <App />
+         </ThemeProvider>
+      </DndProvider>
    </React.StrictMode>
 )
 
